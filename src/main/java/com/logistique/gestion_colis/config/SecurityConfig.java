1c367d5dbaf26cb6ee675fd9be183128e7d55e18
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-17 16:11:46 +0100

    Feat : update security filter chain to permit POST requests for login endpoint

--- src/main/java/com/logistique/gestion_colis/config/SecurityConfig.java
+++ src/main/java/com/logistique/gestion_colis/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.logistique.gestion_colis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,8 +17,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
 
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                .anyRequest().authenticated()
+                );
 
         return http.build();
     }
