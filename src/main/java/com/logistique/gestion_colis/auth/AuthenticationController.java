6f519241ad5d5397a08441e9720fca2fc464b4f4
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-13 11:00:27 +0100

    Feat : add AuthenticationController for user registration and login endpoints

---
+++ src/main/java/com/logistique/gestion_colis/auth/AuthenticationController.java
@@ -0,0 +1,39 @@
+package com.logistique.gestion_colis.auth;
+
+import com.logistique.gestion_colis.dto.AuthenticationRequest;
+import com.logistique.gestion_colis.dto.AuthenticationResponse;
+import com.logistique.gestion_colis.dto.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+
+
+
+
+}
+
